67764f7839a383b3019c30f1eb193aa339dda6d2
paul <paulstuartcochrane@gmail.com>
2022-01-31 13:02:30 +1000

    Completed Build

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ETCH-A-SKETCH
 Part of Odin Project Fundementals course.
+
+First challenge was getting the canvas grid to adjust depending on size (between 10x10 and 100x100)
+
+I could get a very hacky solution by always creating 10000 squares (most needed) + hiding overflow with CSS on the container. This was using Flexbox. But that didn't feel very elegant, not to mentioned its also additional load time to create additional divs.
+
+I refactored the code from Flexbox to Grid which was a good decision. Grid was a good solution using repeat() to dynamically adjust the number of cells in grid with the range input element.
+
+The next challenge was allowing the user to choose RBG, or color selector instead of black to sketch with. This required creating 2 seperate functions. The RGB required an additional function to randomly generate a HEX code and then run that in the for loop of the sketch function.
+
+Would not be complete without gradient background.
+
+Any feedback on the code is always welcome :)
--- app.js
+++ app.js
@@ -1,38 +1,91 @@
-window.onload = () => {
+//global variables
+const sizeSelector = document.getElementById('size-selector');
+const container = document.querySelector('.grid-container');
+const btn = document.querySelector('.btn');
+const colorBtn = document.querySelector('.btn-2');
+const cell = document.createElement('div');
 
-    //Create grid function
-    const createGrid = () => {
-        const createCell = () => {
-            const cell = document.createElement('div');
-            const container = document.querySelector('.grid-container');
-            cell.className = 'cell'
-            container.appendChild(cell)
-        }
-        for (i = 0; i < 400; i++) {
-            createCell()
-        }
-    }
-    createGrid()
+//Sketch function
+function sketch() {
 
-    //Sketch function
-    const gridCell = document.querySelectorAll('.cell');
+    const gridCells = document.querySelectorAll('.cell');
 
-    for (i = 0; i < gridCell.length; i++) {
-        gridCell[i].addEventListener('mouseover', function (event) {
-            event.target.classList.add('sketch')
+    for (i = 0; i < gridCells.length; i++) {
+        gridCells[i].addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = 'black';
         })
     }
+};
 
-    // Clear sketch function
-    function clearSketch() {
-        const cellElements = document.querySelectorAll('.cell');
-        for (i = 0; i < cellElements.length; i++) {
-            cellElements[i].classList.remove('sketch');
-        }
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+};
 
+function colorSketch() {
+    const gridCells = document.querySelectorAll('.cell');
 
-    const btn = document.querySelector('.btn');
-    btn.addEventListener('click', clearSketch)
+    for (i = 0; i < gridCells.length; i++) {
+        gridCells[i].addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = getRandomColor();
+        })
+    }
+};
 
+//color button click event
+colorBtn.addEventListener('click', colorSketch)
+
+//color selector function
+function selectColor() {
+    const colorValue = document.getElementById('color');
+    const gridCells = document.querySelectorAll('.cell');
+
+    for (i = 0; i < gridCells.length; i++) {
+        gridCells[i].addEventListener('mouseover', function (event) {
+            event.target.style.backgroundColor = colorValue.value;
+        })
+    }
 }
+
+//select color btn
+const colorValue = document.getElementById('color');
+colorValue.addEventListener('click', selectColor)
+
+//create grid function
+function createGrid() {
+
+    container.innerHTML = ''; //clearing divs before creating new div
+
+    let gridSize = sizeSelector.value;
+    container.setAttribute('style', `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`)
+    let gridSquareUnit = gridSize * gridSize;
+    for (i = 0; i < gridSquareUnit; i++) {
+        const cell = document.createElement('div');
+        const container = document.querySelector('.grid-container');
+        cell.className = 'cell';
+        container.appendChild(cell);
+    }
+};
+
+//Update grid with size selector
+sizeSelector.addEventListener('mouseup', createGrid);
+sizeSelector.addEventListener('mouseup', sketch);
+
+// Clear sketch function
+function clearSketch() {
+    const cellElements = document.querySelectorAll('.cell');
+    for (i = 0; i < cellElements.length; i++) {
+        cellElements[i].removeAttribute('style');
+    }
+    sketch();
+}
+
+//clear sketch on button click
+btn.addEventListener('click', clearSketch)
+
+//default grid on load
+window.onload = createGrid(), sketch();
